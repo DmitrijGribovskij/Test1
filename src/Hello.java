264509ae0e77745668073a6095695f76210b0cb6
Dmitrij Gribovskij <dmitrijgribovskij@gmail.com>
2018-12-30 12:05:04 +0300

    New

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,6 @@ import java.awt.*;
 
 public class Hello {
     public static void main(String[] args) {
-        Frame frame = new JFrame("MY");
-        frame.setVisible(true);
+        System.out.println("Hello!");
     }
 }
