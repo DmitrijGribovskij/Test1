56fc4a381220bd1a3665feab6e5e831f913d9828
Dmitrij Gribovskij <dmitrijgribovskij@gmail.com>
2018-12-30 09:37:58 +0300

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        Frame frame = new JFrame("MY");
+        frame.setVisible(true);
+    }
+}
